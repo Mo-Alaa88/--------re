71266d462b71bd2ed725dba2ea87a79652af5212
Yarab <judymotors@gmail.com>
2019-02-03 22:03:11 +0200

    Remove commented code

--- js/app.js
+++ js/app.js
@@ -1,30 +1,4 @@
-// Simple Todolist
-// https://mo-alaa88.github.io/simple-Todolist/
-// https://github.com/Mo-Alaa88/simple-Todolist
-////
-// Task
-// https://mo-alaa88.github.io/task/
-// https://github.com/Mo-Alaa88/task
-///
-// Badrit
-// https://mo-alaa88.github.io/badrit/
-// https://github.com/Mo-Alaa88/badrit
-///
-// Apple
-// https://mo-alaa88.github.io/Apple/
-// https://github.com/Mo-Alaa88/Apple
-////
-// Arcade Game
-// https://mo-alaa88.github.io/Arcade-Game/
-// https://github.com/Mo-Alaa88/Arcade-Game
-///
-// Memory Game
-// https://mo-alaa88.github.io/Memory-Game/
-// https://github.com/Mo-Alaa88/Memory-Game
-///
-// Restaurant Stage
-// 
-// https://github.com/Mo-Alaa88/Restaurant-Stage
+
 const nameArray = [`Simple Todolist`,`Task`,`Badrit`,`Apple`,`Arcade Game`,`Memory Game`,`Restaurant Stage`],
     liveArray = [`https://mo-alaa88.github.io/simple-Todolist/`,`https://mo-alaa88.github.io/task/`,`https://mo-alaa88.github.io/badrit/`,`https://mo-alaa88.github.io/Apple/`,`https://mo-alaa88.github.io/Arcade-Game/`,`https://mo-alaa88.github.io/Memory-Game/`,`none`],
     gitArray = [`https://github.com/Mo-Alaa88/simple-Todolist`,`https://github.com/Mo-Alaa88/task`,`https://github.com/Mo-Alaa88/badrit`,`https://github.com/Mo-Alaa88/Apple
@@ -56,52 +30,3 @@ nameArray.forEach((title,index)=>{
     }
 
 });
-// function autoRun (){
-//     // console.log('sfxhdd');
-//     for (let i=0; i<=17; i++){
-
-// //         var sample = [1, 2, 3];
-
-// // // es5
-
-// // sample.forEach(function (elem, index){
-// //    console.log(elem + ' comes at ' + index);
-// // })
-
-// // // es6
-
-// // sample.forEach((elem, index) => `${elem} comes at ${index}`)
-
-// /*
-// output
-
-// 1 comes at 0
-// 2 comes at 1
-// 3 comes at 2
-// */
-//         // if (i%2){
-//         //     console.log('lkjkojhoi');
-//         //     // div.classList.add('blue');
-//         //     const div = `<div class=" blue" id='toggleColor${i}'>
-//         //     <h3 class=' project'>Apple</h3>
-//         //     <p>You can viste online website by this <a href="" title="">link</a></p>
-//         //     <p>Ripo project <a>https</a></p>
-//         //     </div>`;
-//         //     const template = document.getElementById('template').innerHTML = div;
-//         //     document.getElementById('content').innerHTML += template;
-//         // }else{
-//         //     // div.classList.add('mediamBlue');
-//         //     const div = `<div class="mediamBlue " id='toggleColor${i}'>
-//         //     <h3 class=' project'>Apple</h3>
-//         //     <p>You can viste online website by this <a href="" title="">link</a></p>
-//         //     <p>Ripo project <a>https</a></p>
-//         //     </div>`;
-            
-//         //     const template = document.getElementById('template').innerHTML = div;
-//         //     document.getElementById('content').innerHTML += template;
-//         // }
-
-//     } 
-// }
-
-// autoRun();
